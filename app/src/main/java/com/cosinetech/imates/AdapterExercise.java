a8b23df6f693b714a7806b6416dec39f0410ecb3
Dingtao <dingtao.cpp@foxmail.com>
2024-12-13 13:22:11 +0800

    refine exercise ui

---
+++ app/src/main/java/com/cosinetech/imates/AdapterExercise.java
@@ -0,0 +1,60 @@
+package com.cosinetech.imates;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cosinetech.imates.model.BiologyQuestion;
+
+import java.util.List;
+
+public class AdapterExercise extends RecyclerView.Adapter<AdapterExercise.ExerciseItemViewHolder> {
+
+    private List<BiologyQuestion> dataList;
+
+    // 构造函数接收数据列表
+    public AdapterExercise(List<BiologyQuestion> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ExerciseItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_exercise, parent, false);
+
+        return new ExerciseItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ExerciseItemViewHolder holder, int position) {
+        BiologyQuestion data = dataList.get(position);
+        holder.itemText.setContent(data.getQuestion());
+
+        // 设置点击监听器
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 处理点击事件
+                // Toast.makeText(v.getContext(), "Clicked on item " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    // ViewHolder静态内部类
+    public static class ExerciseItemViewHolder extends RecyclerView.ViewHolder {
+        MarkdownTextView itemText;
+        ExerciseItemViewHolder(View view) {
+            super(view);
+            itemText = view.findViewById(R.id.exercise);
+        }
+    }
+}
--- app/src/main/java/com/cosinetech/imates/FragmentExerciseList.java
+++ app/src/main/java/com/cosinetech/imates/FragmentExerciseList.java
@@ -2,7 +2,12 @@ package com.cosinetech.imates;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,19 +29,12 @@ public class FragmentExerciseList extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private FragmentChatAi fragmentChatAi;
+
     public FragmentExerciseList() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentExerciseList.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentExerciseList newInstance(String param1, String param2) {
         FragmentExerciseList fragment = new FragmentExerciseList();
         Bundle args = new Bundle();
@@ -60,5 +58,15 @@ public class FragmentExerciseList extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_exercise_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fragmentChatAi = FragmentChatAi.newInstance(EnumApiUrl.URL_CHAT_BIOLOGY, false);
+        getChildFragmentManager().beginTransaction()
+                .replace(R.id.fragmentChatAiContainer, fragmentChatAi)
+                .addToBackStack(null) // 添加到回退栈以便用户可以返回
+                .commit();
     }
 }
