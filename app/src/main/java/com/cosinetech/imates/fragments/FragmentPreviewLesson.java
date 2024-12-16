bd040988e6a29f63c4dce099d884ac49dfd6c995
Dingtao <dingtao.cpp@foxmail.com>
2024-12-16 23:11:35 +0800

    add pdf and rating dependency

--- app/src/main/java/com/cosinetech/imates/fragments/FragmentPreviewLesson.java
+++ app/src/main/java/com/cosinetech/imates/fragments/FragmentPreviewLesson.java
@@ -10,8 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cosinetech.imates.R;
+import com.cosinetech.imates.activities.MainActivity;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+import com.github.spareyaya.SimpleRatingView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -81,5 +86,50 @@ public class FragmentPreviewLesson extends Fragment {
 
         TextView textPreviewSectionName = view.findViewById(R.id.preview_section_name);
         textPreviewSectionName.setText(mPreviewSectionName);
+
+        SimpleRatingView ratingView = view.findViewById(R.id.rating);
+        ratingView.setOnRatingChangeListener((oldRating, newRating) ->  {
+            Toast.makeText(getContext(), "oldRating:" + oldRating + " newRating:" + newRating, Toast.LENGTH_SHORT).show();
+        });
+
+        SimpleRatingView ratingDifficult = view.findViewById(R.id.rating_difficulty);
+        ratingDifficult.setOnRatingChangeListener((oldRating, newRating) -> {
+            Toast.makeText(getContext(), "diff: oldRating:" + oldRating + " newRating:" + newRating, Toast.LENGTH_SHORT).show();
+        });
+
+        PDFView pdfView = view.findViewById(R.id.pdfView);
+        pdfView.fromAsset("biology/chapter5/text_book.pdf")
+                //.pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                // allows to draw something on the current page, usually visible in the middle of the screen
+                //.onDraw(onDrawListener)
+                // allows to draw something on all pages, separately for every page. Called only for visible pages
+//                .onDrawAll(onDrawListener)
+//                .onLoad(onLoadCompleteListener) // called after document is loaded and starts to be rendered
+//                .onPageChange(onPageChangeListener)
+//                .onPageScroll(onPageScrollListener)
+//                .onError(onErrorListener)
+//                .onPageError(onPageErrorListener)
+//                .onRender(onRenderListener) // called after document is rendered for the first time
+                // called on single tap, return true if handled, false to toggle scroll handle visibility
+//                .onTap(onTapListener)
+//                .onLongPress(onLongPressListener)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+                ///.linkHandler(DefaultLinkHandler)
+                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
+                .pageSnap(false) // snap pages to screen boundaries
+                .pageFling(false) // make a fling change only a single page like ViewPager
+                .nightMode(false) // toggle night mode
+                .load();
     }
 }
