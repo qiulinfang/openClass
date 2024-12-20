fb1bf2a24dd174cc7b3ef8f28f2bf962281c28bc
Dingtao <dingtao.cpp@foxmail.com>
2024-12-20 13:07:40 +0800

    note worked

--- app/src/main/java/com/cosinetech/imates/notes/NoteView.java
+++ app/src/main/java/com/cosinetech/imates/notes/NoteView.java
@@ -3,13 +3,8 @@ package com.cosinetech.imates.notes;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,19 +14,17 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.cosinetech.imates.R;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebSettings;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
-
-import androidx.webkit.WebViewAssetLoader;
+import com.cosinetech.imates.util.ImageUtils;
+import com.cosinetech.imates.util.ScreenUtils;
+import com.cosinetech.imates.util.StringUtils;
+import com.sendtion.xrichtext.RichTextEditor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NoteView extends LinearLayout {
     private ListView noteListView;
-    private WebView webView;
+    private RichTextEditor xRichText;
     private EditText etTitle;
     private Button newNoteButton;
     private Button saveNoteButton;
@@ -56,16 +49,8 @@ public class NoteView extends LinearLayout {
 
     private void init(Context context) {
         View view = LayoutInflater.from(context).inflate(R.layout.note_view_layout, this, true);
-
-        webView = view.findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.addJavascriptInterface(new JavaScriptInterface(context), "Android");
-        webView.loadUrl("file:///android_asset/editor.html");
-
-
         noteListView = view.findViewById(R.id.noteListView);
+        xRichText = view.findViewById(R.id.text_area);
         newNoteButton = view.findViewById(R.id.newNoteButton);
         saveNoteButton = view.findViewById(R.id.saveNoteButton);
         etTitle = view.findViewById(R.id.et_title);
@@ -79,15 +64,36 @@ public class NoteView extends LinearLayout {
         noteListView.setOnItemClickListener((parent, view1, position, id) -> {
             currentNoteId = noteManager.getNoteByPosition(position).getId();
             Note selectedNote = noteManager.getNoteById(currentNoteId);
-            selectedNote.getContent();
-
+            xRichText.clearAllLayout();
+            List<String> textList = StringUtils.cutStringByImgTag(selectedNote.getContent());
+            for (int i = 0; i < textList.size(); i++) {
+                String text = textList.get(i);
+                if (text.contains("<img")) {
+                    String imagePath = StringUtils.getImgSrc(text);
+//                    int width = ScreenUtils.getScreenWidth(this);
+//                    int height = ScreenUtils.getScreenHeight(this);
+                    xRichText.measure(0,0);
+                    Bitmap bitmap = ImageUtils.getDecodeBitmap(imagePath);//ImageUtils.getSmallBitmap(imagePath, width, height);
+//                    int width = bitmap.getWidth();
+//                    int height = bitmap.getHeight();
+                    if (bitmap != null){
+                        xRichText.addImageViewAtIndex(xRichText.getLastIndex(), imagePath);
+                    } else {
+                        xRichText.addEditTextAtIndex(xRichText.getLastIndex(), text);
+                    }
+                    xRichText.addEditTextAtIndex(xRichText.getLastIndex(), "");
+                }
+                else {
+                    xRichText.addEditTextAtIndex(xRichText.getLastIndex(), text);
+                }
+            }
         });
 
         newNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 currentNoteId = -1;
-
+                xRichText.clearAllLayout();
             }
         });
 
@@ -95,8 +101,15 @@ public class NoteView extends LinearLayout {
             @Override
             public void onClick(View v) {
                 String title = etTitle.getText().toString().trim();
+                List<RichTextEditor.EditData> editList = xRichText.buildEditData();
                 StringBuffer contentBuilder = new StringBuffer();
-
+                for (RichTextEditor.EditData itemData : editList) {
+                    if (itemData.inputStr != null) {
+                        contentBuilder.append(itemData.inputStr);
+                    } else if (itemData.imagePath != null) {
+                        contentBuilder.append("<img src=\"").append(itemData.imagePath).append("\"/>");
+                    }
+                }
 
                 String content = contentBuilder.toString();
                 if (currentNoteId == -1) {
@@ -116,29 +129,4 @@ public class NoteView extends LinearLayout {
         adapter.addAll(noteTitles);
         adapter.notifyDataSetChanged();
     }
-
-
-    // JavaScript interface for communication
-    private class JavaScriptInterface {
-        private Context context;
-
-        JavaScriptInterface(Context context) {
-            this.context = context;
-        }
-
-        @JavascriptInterface
-        public void saveContent(String content) {
-            Log.d("NOTE", content);
-        }
-
-        @JavascriptInterface
-        public void loadContent() {
-            String content = "asdfsdf";
-            webView.post(() -> webView.evaluateJavascript(
-                    "setLoadedContent('" + content.replace("'", "\\'") + "');",
-                    null
-            ));
-        }
-    }
-
 }
