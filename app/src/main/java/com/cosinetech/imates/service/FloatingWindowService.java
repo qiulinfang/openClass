694be4858ca37472edbdb0370764e45456294b85
Dingtao <dingtao.cpp@foxmail.com>
2024-12-27 20:11:38 +0800

    2024.12.27 临时禁用机器人拖动

--- app/src/main/java/com/cosinetech/imates/service/FloatingWindowService.java
+++ app/src/main/java/com/cosinetech/imates/service/FloatingWindowService.java
@@ -119,30 +119,30 @@ public class FloatingWindowService extends Service {
         });
 
         // 使悬浮窗可拖动
-        floatingRobotView.setOnTouchListener(new View.OnTouchListener() {
-            private int initialX;
-            private int initialY;
-            private float initialTouchX;
-            private float initialTouchY;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        initialX = layoutParams.x;
-                        initialY = layoutParams.y;
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        layoutParams.x = initialX + (int) (event.getRawX() - initialTouchX);
-                        layoutParams.y = initialY + (int) (event.getRawY() - initialTouchY);
-                        windowManager.updateViewLayout(floatingRobotView, layoutParams);
-                        return true;
-                }
-                return true;
-            }
-        });
+//        floatingRobotView.setOnTouchListener(new View.OnTouchListener() {
+//            private int initialX;
+//            private int initialY;
+//            private float initialTouchX;
+//            private float initialTouchY;
+//
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                        initialX = layoutParams.x;
+//                        initialY = layoutParams.y;
+//                        initialTouchX = event.getRawX();
+//                        initialTouchY = event.getRawY();
+//                        return true;
+//                    case MotionEvent.ACTION_MOVE:
+//                        layoutParams.x = initialX + (int) (event.getRawX() - initialTouchX);
+//                        layoutParams.y = initialY + (int) (event.getRawY() - initialTouchY);
+//                        windowManager.updateViewLayout(floatingRobotView, layoutParams);
+//                        return true;
+//                }
+//                return true;
+//            }
+//        });
     }
 
     public void popupChatBot1(String url, String tag) {
