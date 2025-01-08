bd2fdeef2d75f9463f70e435709ae35e7f8fd571
Dingtao <dingtao.cpp@foxmail.com>
2025-01-08 11:31:20 +0800

    beauty ui

--- app/src/main/java/com/cosinetech/imates/fragments/FragmentQuestionList.java
+++ app/src/main/java/com/cosinetech/imates/fragments/FragmentQuestionList.java
@@ -90,7 +90,7 @@ public class FragmentQuestionList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_question_list, container, false);
+        return inflater.inflate(R.layout.fragment_question_solve, container, false);
     }
 
     @Override
