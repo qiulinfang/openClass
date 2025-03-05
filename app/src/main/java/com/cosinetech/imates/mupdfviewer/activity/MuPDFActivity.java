3b6c782d5f4b956aa0377bc679afda798b70f731
Dingtao <dingtao.cpp@foxmail.com>
2025-03-05 11:02:23 +0800

    feat: 修改MuPdf背景色为灰色

--- app/src/main/java/com/cosinetech/imates/mupdfviewer/activity/MuPDFActivity.java
+++ app/src/main/java/com/cosinetech/imates/mupdfviewer/activity/MuPDFActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
