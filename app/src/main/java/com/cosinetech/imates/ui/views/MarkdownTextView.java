fcf570ade85ab2badc1147cd6eabc1712c8259c0
Dingtao <dingtao.cpp@foxmail.com>
2025-08-19 17:57:17 +0800

    feat: 更新markwon, 带缓存
          - 待完成: 截图问答待传参
          - 待完成: 套餐学习情况, 分用户记录
          - 待完成: 新消息小红点
          - 待完成: 消息失败提示
          - 待完成: 消息重发
          - 待完成: 消息平滑刷新

--- app/src/main/java/com/cosinetech/imates/ui/views/MarkdownTextView.java
+++ app/src/main/java/com/cosinetech/imates/ui/views/MarkdownTextView.java
@@ -51,7 +51,7 @@ public class MarkdownTextView extends AppCompatTextView {
                 .usePlugin(JLatexMathPlugin.create(this.getTextSize(), builder -> {
                     // enable inlines (require `MarkwonInlineParserPlugin`), by default `false`
                     builder.inlinesEnabled(true);
-                    builder.allowInlinesSingleDollar(true);
+                    //builder.allowInlinesSingleDollar(true);
                 }))
                 .usePlugin(TablePlugin.create(context))
                 .build();
