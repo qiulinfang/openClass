58b4d7cc9c2fe26f748a28715ffb22348e22fb92
Dingtao <dingtao.cpp@foxmail.com>
2025-08-25 09:26:02 +0800

    bugfix: 修复由于缓存导致的消息显示错乱问题
          - 待完成: 截图问答待传参
          - 待完成: 套餐学习情况, 分用户记录
          - 待完成: 新消息小红点
          - 待完成: 消息失败提示
          - 待完成: 消息重发
          - 待完成: 消息平滑刷新

--- app/src/main/java/com/cosinetech/imates/ui/views/ChatAiView.java
+++ app/src/main/java/com/cosinetech/imates/ui/views/ChatAiView.java
@@ -533,7 +533,7 @@ public class ChatAiView extends RelativeLayout {
         });
 
         // Initialize RecyclerView
-        mMsgDetailListView.setItemViewCacheSize(-100);
+        mMsgDetailListView.setItemViewCacheSize(20);
         mMsgDetailListView.setItemAnimator(null);
         mMsgDetailListView.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
         mMsgDetailListView.addItemDecoration(new RecyclerViewOverscrollDecoration());
