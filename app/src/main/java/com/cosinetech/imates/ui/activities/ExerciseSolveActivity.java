1195f82aaafe74472644f31a2b5a7f1e9507a6e2
Dingtao <dingtao.cpp@foxmail.com>
2025-09-26 11:46:16 +0800

    bugfix: 修复pdf界面多余的工具按钮
          - 待完成: 截图问答待传参
          - 待完成: 套餐学习情况, 分用户记录
          - 待完成: 新消息小红点
          - 待完成: 消息失败提示
          - 待完成: 消息重发

--- app/src/main/java/com/cosinetech/imates/ui/activities/ExerciseSolveActivity.java
+++ app/src/main/java/com/cosinetech/imates/ui/activities/ExerciseSolveActivity.java
@@ -364,17 +364,18 @@ public class ExerciseSolveActivity extends AppCompatActivity implements Messagin
     }
 
     private String getMiniClassUrl() {
-        String prefix = subject.name();
-        try {
-            if(mCurrentQuestionIndex >= 0 && mCurrentQuestionIndex < mQuestions.size()){
-                String id =  mQuestions.get(mCurrentQuestionIndex).bmNo;
-                return "https://www.imates.com.cn:9099/miniclass/" + prefix + "/" + id + ".html";
-            } else {
-                return "";
-            }
-        } catch (Exception e) {
-            return "";
-        }
+        return "https://www.imates.com.cn:9099/demo/demo1.html";
+//        String prefix = subject.name();
+//        try {
+//            if(mCurrentQuestionIndex >= 0 && mCurrentQuestionIndex < mQuestions.size()){
+//                String id =  mQuestions.get(mCurrentQuestionIndex).bmNo;
+//                return "https://www.imates.com.cn:9099/miniclass/" + prefix + "/" + id + ".html";
+//            } else {
+//                return "";
+//            }
+//        } catch (Exception e) {
+//            return "";
+//        }
     }
 
     public static ChatMessageSession createChatTeacherSession(String chatAiSessionId, String chatAiSessionName, ChatMessageSession.SessionType type) {
