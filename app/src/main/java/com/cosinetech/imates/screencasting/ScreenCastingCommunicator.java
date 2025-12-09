ce0309072d2d3315b7b8415ff44be13e6ca6abbd
Dingtao <dingtao.cpp@foxmail.com>
2025-12-09 17:16:47 +0800

    bugfix: 修复大屏和教师pad的ip变化的通知问题
          - 待完成: 截图问答待传参
          - 待完成: 套餐学习情况, 分用户记录
          - 待完成: 新消息小红点
          - 待完成: 消息失败提示
          - 待完成: 消息重发

--- app/src/main/java/com/cosinetech/imates/screencasting/ScreenCastingCommunicator.java
+++ app/src/main/java/com/cosinetech/imates/screencasting/ScreenCastingCommunicator.java
@@ -196,11 +196,13 @@ public class ScreenCastingCommunicator {
                 if(!newInfo.getScreens().isEmpty()) {
                     this.pcDeviceIp = newInfo.getScreens().get(0).getIp();
                     Log.e(TAG, "Screen Device: " + pcDeviceIp);
+                    mainHandler.post(() -> commandHandler.onReceivePcIpAddress(pcDeviceIp));
                 }
 
                 if(!newInfo.getTeachers().isEmpty()) {
                     this.teacherPadDeviceIp = newInfo.getTeachers().get(0).getIp();
                     Log.e(TAG, "TeacherPad Device: " + pcDeviceIp);
+                    mainHandler.post(() -> commandHandler.onReceiveTeacherPadIpAddress(teacherPadDeviceIp));
                 }
             });
 
@@ -325,7 +327,7 @@ public class ScreenCastingCommunicator {
 
                 } catch (Exception e) {
                     Log.e(TAG, "接收消息时出错", e);
-                    notifyNetworkError("接收消息时出错: " + e.getMessage());
+                    //notifyNetworkError("接收消息时出错: " + e.getMessage());
                 }
             }
         });
@@ -353,7 +355,7 @@ public class ScreenCastingCommunicator {
 
                 } catch (Exception e) {
                     Log.e(TAG, "接收消息时出错", e);
-                    notifyNetworkError("接收消息时出错: " + e.getMessage());
+                    //notifyNetworkError("接收消息时出错: " + e.getMessage());
                 }
             }
         });
